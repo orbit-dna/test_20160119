d994d399e503c98c5c8fc6da6a24cb30de9f491c
Jiang QiMing <orbit_dna@163.com>
2016-01-23 19:46:42 +0800

    student的show/update
    如题

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,12 +7,11 @@ class StudentsController < ApplicationController
 		index
 		@student=Student.new(get_student_params)
 		sch_class=nil
-		# render plain: [@sch_class.inspect, @student.inspect]
 		id=params.require(:sch_class)[:id]
 		begin
 			sch_class=SchClass.find(id)
 		rescue
-			@student.errors[:all] << "指定班级不存在"
+			@student.errors[:base] << "指定班级不存在"
 		end unless id.blank?
 		if @student.errors.count == 0 and @student.save
 			sch_class.students.append(@student) unless sch_class.blank?
@@ -21,9 +20,40 @@ class StudentsController < ApplicationController
 		# sch_class=nil
 		render 'index'
 	end 
-	def destroy
+	def show
+		begin
+			@student=Student.find(params[:id])
+			@sch_class=@student.sch_class
+		rescue ActiveRecord::RecordNotFound
+			index
+			@student.errors[:base] << "student #{params[:id]} does not exists"
+			render 'index'
+		end
+	end
+	def update
+		@sch_class=nil
+		id=params.require(:sch_class)[:id]
+		begin
+			@sch_class=SchClass.find(id)
+		rescue
+			@student.errors[:base] << "指定班级不存在"
+		end unless id.blank?
 		@student=Student.find(params[:id])
-		@student.destroy
+		@student.update(get_student_params)
+		if @sch_class.blank?
+			origin_class=@student.sch_class
+			origin_class.students.delete @student unless origin_class.blank?
+		else
+			@sch_class.students.append(@student) 
+		end
+		render 'show'
+	end
+	def destroy
+		begin
+			@student=Student.find(params[:id])
+			@student.destroy
+		rescue ActiveRecord::RecordNotFound
+		end
 		redirect_to students_path
 	end
 
