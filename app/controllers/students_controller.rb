90225d05b987d6c21c58899ee60eb3b6c0ba3359
Jiang QiMing <orbit_dna@163.com>
2016-01-23 17:24:56 +0800

    添加student的delete
    如题

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,21 +6,26 @@ class StudentsController < ApplicationController
 	def create
 		index
 		@student=Student.new(get_student_params)
-		@sch_class=nil
+		sch_class=nil
 		# render plain: [@sch_class.inspect, @student.inspect]
 		id=params.require(:sch_class)[:id]
 		begin
-			@sch_class=SchClass.find(id)
+			sch_class=SchClass.find(id)
 		rescue
 			@student.errors[:all] << "指定班级不存在"
 		end unless id.blank?
 		if @student.errors.count == 0 and @student.save
-			@sch_class.students.append(@student) unless @sch_class.blank?
+			sch_class.students.append(@student) unless sch_class.blank?
 			@student=Student.new
 		end
-		# @sch_class=nil
+		# sch_class=nil
 		render 'index'
 	end 
+	def destroy
+		@student=Student.find(params[:id])
+		@student.destroy
+		redirect_to students_path
+	end
 
 	private 
 	def get_student_params
