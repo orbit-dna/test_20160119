3f52e842a67ecb5ee4f408b543644a7de1d8b852
Jiang QiMing <orbit_dna@163.com>
2016-01-23 17:04:38 +0800

    添加学生student的index/create
    如题

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,29 @@
 class StudentsController < ApplicationController
+	def index
+		@students=Student.all
+		@student=Student.new
+	end
+	def create
+		index
+		@student=Student.new(get_student_params)
+		@sch_class=nil
+		# render plain: [@sch_class.inspect, @student.inspect]
+		id=params.require(:sch_class)[:id]
+		begin
+			@sch_class=SchClass.find(id)
+		rescue
+			@student.errors[:all] << "指定班级不存在"
+		end unless id.blank?
+		if @student.errors.count == 0 and @student.save
+			@sch_class.students.append(@student) unless @sch_class.blank?
+			@student=Student.new
+		end
+		# @sch_class=nil
+		render 'index'
+	end 
+
+	private 
+	def get_student_params
+		params.require(:student).permit(:name, :gender, :birth)
+	end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ActiveRecord::Base
   belongs_to :sch_class
 
-  validates :name, presence: true, length: { mininum: 3}
+  validates :name, presence: true, length: { minimum: 3}
 end
