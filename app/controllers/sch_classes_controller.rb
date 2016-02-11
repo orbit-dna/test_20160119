af59fa4aab60b2a57b6ba7a7498d1539638cefe3
Me <me@debian-box>
2016-02-11 13:35:23 +0800

    extract pagination to a helper
    pagination functions which was implemented separately in
    different module now are extracted to form a single helper
    in ApplicationHepler.paginate

--- app/controllers/sch_classes_controller.rb
+++ app/controllers/sch_classes_controller.rb
@@ -1,9 +1,7 @@
 class SchClassesController < ApplicationController
 	def index
 		@sch_class=SchClass.new
-		@sch_classes,@page_range=
-			paginatie (SchClass.all.sort_by {|x| [x.grade,x.class_number]}),
-				params[:page].to_i, @@items_per_page, sch_classes_path
+		@sch_classes=SchClass.all.sort_by {|x| [x.grade, x.class_number]}
 	end
 	def create
 		index
@@ -16,10 +14,7 @@ class SchClassesController < ApplicationController
 	def show
 		begin
 			@sch_class=SchClass.find(params[:id])
-			@students,@page_range=
-				paginatie (@sch_class.students.sort_by {|x| x.id}),
-					params[:page].to_i, @@items_per_page,
-				   	sch_class_path(@sch_class)
+			@students=@sch_class.students.sort_by {|x| x.id}
 		rescue ActiveRecord::RecordNotFound
 			index
 			@sch_class.errors[:base] << "class #{params[:id]} does not exists"
@@ -46,22 +41,5 @@ class SchClassesController < ApplicationController
 	@@items_per_page=5
 	def get_sch_class_params
 		params.require(:sch_class).permit(:grade, :class_number, :teacher)
-	end
-	def paginatie(datas,page_id,items_per_page,base_path)
-		page_max=(datas.count-1)/items_per_page
-		page_max=0 if page_max < 0
-		page_id-=1
-		page_id=0 if page_id < 0
-		page_id=page_max if page_id > page_max
-		range_current=page_id+1
-		range_start=range_current-2
-		range_start=1 if range_start < 1
-		range_end=range_current+2
-		range_end=page_max+1 if range_end > page_max+1
-		range_prev=page_id==0 ? nil : range_current-1
-		range_next=page_id==page_max ? nil : range_current+1
-		return datas[page_id*items_per_page,items_per_page],
-			{current: range_current, start: range_start, end: range_end,
-				prev: range_prev, next: range_next, base_path: base_path }
 	end
 end
